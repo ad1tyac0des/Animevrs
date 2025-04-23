02e1023021c092ac6cd628562539c503b1d67b41
Aditya Singh <adityasingh740745@gmail.com>
2025-04-23 15:40:12 +0530

    feat(hamburger-menu): add hamburger panel for mobile navigation with animations; implement open/close functionality and update styles

--- js/main.js
+++ js/main.js
@@ -1,8 +1,10 @@
 // Import modules
 import { preloader } from './modules/preloader.js';
 import { handleSearchPanel } from './modules/handleSearchPanel.js';
+import { handleHamburgerMenu } from './modules/handleHamburgerMenu.js';
 
 document.addEventListener('DOMContentLoaded', () => {
     // preloader();
     handleSearchPanel();
+    handleHamburgerMenu();
 });
---
+++ js/modules/handleHamburgerMenu.js
@@ -0,0 +1,90 @@
+import { createTimeline, stagger } from 'animejs';
+
+export function handleHamburgerMenu() {
+    const hamburgerMenu = document.querySelector("#hamburger-menu");
+    const hamburgerPanelCloseButton = document.querySelector("#hamburger-panel-close-button");
+
+    let isOpen = false;
+
+    function toggleBodyScroll() {
+        if (isOpen) {
+            // Disable scrolling
+            document.body.style.overflow = "hidden";
+            document.body.style.height = "100vh";
+        } else {
+            // Enable scrolling
+            document.body.style.overflow = "";
+            document.body.style.height = "";
+        }
+    }
+
+    // opens hamburger panel
+    function openHamburgerMenu() {
+        const tl = createTimeline();
+
+        tl.add("#hamburger-panel-container", {
+            translateX: ["100%", "0%"],
+            opacity: [0, 1],
+            duration: 500,
+            ease: "inOutSine"
+        }).add("#hamburger-panel-links a", {
+            opacity: [0, 1],
+            x: [30, 0],
+            duration: 400,
+            // delay: stagger(20),
+            ease: "inOutSine"
+        }).add(["#hamburger-panel-close-line-1", "#hamburger-panel-close-line-2"], {
+            width: [0, "100%"],
+            duration: 500,
+            ease: "inOutSine"
+        }).add("#hamburger-panel-footer-container p", {
+            opacity: [0, 1],
+            duration: 500,
+            ease: "inOutSine",
+            delay: stagger(200)
+        });
+    }
+
+    // close hamburger panel
+    function closeHamburgerMenu() {
+        const tl = createTimeline({
+            reversed: true,
+        });
+
+        tl.add("#hamburger-panel-container", {
+            translateX: ["100%", "0%"],
+            opacity: [0, 1],
+            duration: 400,
+            ease: "inOutSine"
+        }).add("#hamburger-panel-links a", {
+            opacity: [0, 1],
+            x: [30, 0],
+            duration: 300,
+            // delay: stagger(20),
+            ease: "inOutSine"
+        }).add(["#hamburger-panel-close-line-1", "#hamburger-panel-close-line-2"], {
+            width: [0, "100%"],
+            duration: 400,
+            ease: "inOutSine"
+        }).add("#hamburger-panel-footer-container p", {
+            opacity: [0, 1],
+            duration: 200,
+            ease: "inOutSine",
+            delay: stagger(200)
+        })
+    }
+
+    // open hamburger panel on clicking menu button
+    hamburgerMenu.addEventListener("click", () => {
+        openHamburgerMenu();
+        isOpen = true;
+        toggleBodyScroll();
+    });
+
+    // close hamburger panel on clicking close button
+    hamburgerPanelCloseButton.addEventListener("click", () => {
+        closeHamburgerMenu();
+        isOpen = false;
+        toggleBodyScroll();
+    });
+}
